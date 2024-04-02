74ca846b454bb223637c3b39ee8b66414ed24e19
Luis Payan <luis.payan1200@gmail.com>
2024-04-02 16:46:10 -0400

    part 2 complete, add serializer, add models file add routes in URL file, add view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Shoe
+from.serializers import ShoeSerializer
 
 # Create your views here.
 
@@ -7,3 +10,12 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the shoe-collector api home route!'}
         return Response(content)
+
+class ShoeList(generics.ListCreateAPIView):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
+
+class ShoeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
+    lookup_field = 'id'
